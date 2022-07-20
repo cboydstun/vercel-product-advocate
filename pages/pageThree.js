20d2211e32c96adc7b673992115495bd5b3af95f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2022-07-20 06:22:00 -0500

    syntax bug

--- pages/pageThree.js
+++ pages/pageThree.js
@@ -16,7 +16,7 @@ export default function PageThree() {
           Question Three
         </h1>
         <p className={styles.description}>Using Next.js</p>
-        <Link href="/hello-vercel">Click Here for Redirect from '/hello-vercel' to vercel.com</Link>
+        <Link href="/hello-vercel">Click Here for Redirect from /hello-vercel to vercel.com</Link>
       </main>
       <footer className={styles.footer}>
         <Link href='/pageFour'>Next Question</Link>
